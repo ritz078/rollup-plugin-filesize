ee429bcce00d4dd3f2130fa43871946f8ac88eaf
Ritesh Kumar <rkritesh078@gmail.com>
2016-01-27 17:10:26 +0530

    added build files

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function _interopDefault (ex) { return 'default' in ex ? ex['default'] : ex; }
+
+var fileSize = _interopDefault(require('filesize'));
+var boxen = _interopDefault(require('boxen'));
+var Chalk = _interopDefault(require('chalk'));
+
+var chalk = new Chalk({ enabled: true });
+
+function filesize() {
+    return {
+        transformBundle: function transformBundle(code) {
+            console.log(boxen(chalk.green.bold('Bundle size : ') + chalk.yellow.bold(fileSize(Buffer.byteLength(code))), { padding: 1 }));
+        }
+    };
+};
+
+module.exports = filesize;
