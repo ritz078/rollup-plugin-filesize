66af463bcf8361f33db0da70609884569ac5aa50
Ritesh Kumar <rkritesh078@gmail.com>
2016-04-26 23:21:30 +0530

    fix package

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-    external: [ 'filesize', 'boxen', 'chalk', 'lodash' ],
+    external: [ 'filesize', 'boxen', 'chalk', 'deep-assign' ],
     plugins: [
         babel({
 					babelrc:false,
