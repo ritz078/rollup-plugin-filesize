66bbbbf3e7b53b1746421e7aacc0eab100bb8985
Ritesh Kumar <rkritesh078@gmail.com>
2017-06-21 15:02:32 +0530

    fixes

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
-import fileSize from 'filesize';
-import boxen from 'boxen';
-import colors from 'colors';
-import deepAssign from 'deep-assign';
-import gzip from 'gzip-size';
+import fileSize from "filesize";
+import boxen from "boxen";
+import colors from "colors";
+import deepAssign from "deep-assign";
+import gzip from "gzip-size";
 
-function render (opt, size, gzip, bundle) {
-	const primaryColor = opt.theme === 'dark' ? 'green' : 'black'
-	const secondaryColor = opt.theme === 'dark' ? 'yellow' : 'blue'
+function render(opt, size, gzip, bundle) {
+	const primaryColor = opt.theme === "dark" ? "green" : "black";
+	const secondaryColor = opt.theme === "dark" ? "yellow" : "blue";
 
 	return boxen(
-			`${bundle.dest ? (colors[primaryColor].bold('Destination: ') + colors[secondaryColor](bundle.dest) + '\n') : ''}${colors[primaryColor].bold('Bundle size: ')}${colors[secondaryColor](size)}${opt.showGzippedSize ? (', ' +
-				colors[primaryColor].bold('Gzipped size: ') + colors[secondaryColor](gzip)) : ''}`, { padding: 1 }
-		)
+		`${bundle.dest
+			? colors[primaryColor].bold("Destination: ") +
+					colors[secondaryColor](bundle.dest) +
+					"\n"
+			: ""}${colors[primaryColor].bold("Bundle size: ")}${colors[
+			secondaryColor
+		](size)}${opt.showGzippedSize
+			? ", " +
+					colors[primaryColor].bold("Gzipped size: ") +
+					colors[secondaryColor](gzip)
+			: ""}`,
+		{ padding: 1 }
+	);
 }
 
-export default function filesize (options = {}) {
-
+export default function filesize(options = {}) {
 	let defaultOptions = {
 		format: {},
-		theme: 'dark',
+		theme: "dark",
 		render: render,
 		showGzippedSize: true
 	};
@@ -29,14 +38,16 @@ export default function filesize (options = {}) {
 	}
 
 	return {
-		getData (bundle, code) {
+		getData(bundle, code) {
 			let size = fileSize(Buffer.byteLength(code), opts.format);
-			let gzipSize = fileSize(gzip.sync(code), opts.format);
+			let gzipSize = opts.showGzippedSize
+				? fileSize(gzip.sync(code), opts.format)
+				: "";
 			return opts.render(opts, size, gzipSize, bundle);
 		},
 
-		ongenerate(bundle, { code }){
+		ongenerate(bundle, { code }) {
 			console.log(this.getData(bundle, code));
 		}
-	}
-};
+	};
+}
