25ac00b43b9f8910f16430ee88793a13d225447e
ritz078 <rkritesh078@gmail.com>
2018-07-20 21:15:45 +0530

    replace gzip with minified and gzipped

--- test/index.test.js
+++ test/index.test.js
@@ -3,14 +3,14 @@ import filesize from '../src';
 
 const x = filesize({}, "test");
 
-const code = 'Abaculuss velum in secundus cirpi! Sunt amicitiaes imperium peritus, bassus exemplares.';
+const code = 'function add(first, second) { return first + second; }';
 
 test('fileSize should return a string', t => {
 	t.is(typeof x({dest: 'abc.js'}, code), 'string');
 });
 
 test('fileSize should return correct string', t => {
-	t.true(x({dest: 'abc.js'}, code).indexOf('87') >= 0)
+	t.true(x({dest: 'abc.js'}, code).indexOf('54') >= 0)
 });
 
 test('fileSize should apply correct template', t => {
@@ -21,6 +21,6 @@ test('fileSize should apply correct template', t => {
 	};
 
 	const z = filesize(options, "test");
-	const expected = '87 B';
+	const expected = '54 B';
 	t.is(z({dest: 'abc.js'}, code), expected)
 });
