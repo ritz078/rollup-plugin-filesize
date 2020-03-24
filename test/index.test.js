4d4a85d16203a77a3f0d1f395b619d3780dfc8ca
kimulaco <kmrkdev@gmail.com>
2020-03-24 10:41:04 +0900

    fix: colon style

--- test/index.test.js
+++ test/index.test.js
@@ -1,22 +1,22 @@
-import test from 'ava';
-import filesize from '../src';
+import test from "ava";
+import filesize from "../src";
 
 const x = filesize({}, "test");
 
 const bundle = {
-	fileName: 'bundled-file.js',
-	code: 'function add(first, second) { return first + second; }',
+	fileName: "bundled-file.js",
+	code: "function add(first, second) { return first + second; }",
 };
 
-test('fileSize should return a string', t => {
-	t.is(typeof x({dest: 'abc.js'}, bundle), 'string');
+test("fileSize should return a string", t => {
+	t.is(typeof x({dest: "abc.js"}, bundle), "string");
 });
 
-test('fileSize should return correct string', t => {
-	t.true(x({dest: 'abc.js'}, bundle).indexOf('54') >= 0)
+test("fileSize should return correct string", t => {
+	t.true(x({dest: "abc.js"}, bundle).indexOf("54") >= 0)
 });
 
-test('fileSize should apply correct template', t => {
+test("fileSize should apply correct template", t => {
 	const options = {
 		render: function (opts, bundle, { gzipSize }) {
 			return gzipSize;
@@ -24,7 +24,7 @@ test('fileSize should apply correct template', t => {
 	};
 
 	const z = filesize(options, "test");
-	const expected = '49 B';
-	console.log(z({dest: 'abc.js'}, bundle))
-	t.is(z({dest: 'abc.js'}, bundle), expected)
+	const expected = "49 B";
+	console.log(z({dest: "abc.js"}, bundle))
+	t.is(z({dest: "abc.js"}, bundle), expected)
 });
