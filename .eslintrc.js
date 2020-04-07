4d2269ffa0c644aa09a32d7b64c9f58fdf0efa45
Brett Zamir <brettz9@yahoo.com>
2020-04-08 00:36:35 +0800

    chore: add `.eslintrc.*` with recommended and prettier plugin config (#60)

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+"use strict";
+module.exports = {
+	env: {
+		es6: true,
+		node: true
+	},
+	extends: ["eslint:recommended", "plugin:prettier/recommended"],
+	parserOptions: {
+		ecmaVersion: 2017,
+		sourceType: "module"
+	}
+};
