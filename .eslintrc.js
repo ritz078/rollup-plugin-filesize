0e83344fb3e2e8800d4d2ae2f1452c548b51716f
Brett Zamir <brettz9@yahoo.com>
2021-02-27 18:34:29 +0800

    - npm: Update babel-eslint to non-deprecated babel/eslint-parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,10 @@ module.exports = {
 		es6: true,
 		node: true,
 	},
-	parser: "babel-eslint",
+	parser: "@babel/eslint-parser",
 	extends: ["eslint:recommended", "plugin:prettier/recommended"],
 	parserOptions: {
+		requireConfigFile: false,
 		ecmaVersion: 2017,
 		sourceType: "module",
 	},
